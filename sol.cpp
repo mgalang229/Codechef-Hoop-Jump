3837e3c6255daca4c648d9fec911d5ea8880c56a
Jason Galang <51401355+mgalang229@users.noreply.github.com>
2021-06-03 11:19:58 +0800

    Add files via upload

---
+++ sol.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	int tt;
+	cin >> tt;
+	while (tt--) {
+		int n;
+		cin >> n;
+		// calculate the midpoint from 1 to 'n'
+		cout << (1 + n) / 2 << '\n';
+	}
+	return 0;
+}
